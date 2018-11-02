5a96eeb4eabe2922e1e66d6ea0caa08f06644e12
yasmeleis <ymeleis@gmail.com>
2018-11-02 13:40:51 -0400

    my code part two

--- Challenge:  Files, Loops, and Conditional Logic-104.py
+++ Challenge:  Files, Loops, and Conditional Logic-104.py
@@ -27,19 +27,23 @@ print(nested_list)
 print(nested_list[0:5])
 numerical_list = []
 for num in nested_list:
-    name = num[0]
-    number = float(num[1])
-    numerical_list.append([name, number])
+    print(num)
+    print(len(num))
+    if len(num) == 2:
+        name = num[0]
+        number = float(num[1])
+        numerical_list.append([name, number])
 print(numerical_list[0:5])
-    
+
 
 ## 7. Filter the List ##
 
 # The last value is ~100 people
 numerical_list[len(numerical_list)-1]
 thousand_or_greater = []
+count = 0
 for num in numerical_list:
     if num[1] >= 1000:
         thousand_or_greater.append(num[0])
-    count += 1 
+    count += 1
 print(thousand_or_greater[0:10])
